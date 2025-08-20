c51f4118ad319123328753173ededa679de69019
KishoreGanesh411 <kishoreganeshk14@gmail.com>
2025-08-20 17:59:25 +0530

    ..

--- src/app/lead/dto/update-leadd.input.ts
+++ src/app/lead/dto/update-leadd.input.ts
@@ -1,12 +1,6 @@
 import { Field, ID, InputType, PartialType } from '@nestjs/graphql';
 import { CreateIpkLeaddInput } from './create-lead.input';
 
-
-
-
-
-
-
 @InputType()
 export class UpdateIpkLeaddInput extends PartialType(CreateIpkLeaddInput) {
   @Field(() => ID)
